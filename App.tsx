30dc88824a87eb87aacdc2e03e85c5e22642fe89
VitornscSilva <vitornscsilva.dev@gmail.com>
2023-05-29 16:35:45 -0300

    :tada: start project :tada:

---
+++ App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <View style={{alignSelf: 'center'}}>
+        <Text>Initial configuration</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
