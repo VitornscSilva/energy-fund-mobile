513b10426a538463d7646e98f0ee4b13e6d34775
VitornscSilva <vitornscsilva.dev@gmail.com>
2023-05-29 17:56:07 -0300

    :construction: init app pages  :construction:

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export function Home(): JSX.Element {
+  return (
+    <View style={{alignSelf: 'center'}}>
+      <Text>Home</Text>
+    </View>
+  );
+}
---
+++ src/pages/Trade/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export function Trade(): JSX.Element {
+  return (
+    <View style={{alignSelf: 'center'}}>
+      <Text>Trade</Text>
+    </View>
+  );
+}
