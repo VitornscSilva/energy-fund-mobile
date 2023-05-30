9fe1918d457151869edc6719a38db3d319cd7583
VitornscSilva <vitornscsilva.dev@gmail.com>
2023-05-30 14:46:26 -0300

    :sparkles: add divider component :sparkles:

---
+++ src/components/Divider/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+import colors from '../../styles/colors';
+
+export const Container = styled.View`
+  width: 100%;
+  border-bottom-width: 1px;
+  border-color: ${colors.gray};
+`;
