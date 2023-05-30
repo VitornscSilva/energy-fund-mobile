687495c38469eb056d36bd21255405f165885efe
VitornscSilva <vitornscsilva.dev@gmail.com>
2023-05-30 16:14:26 -0300

    :sparkles: add TabIcons :sparkles:

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,35 +1,71 @@
 import React from 'react';
+import {View} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import SvgUri from 'react-native-svg-uri';
 
 import {Home} from '../pages/Home';
 import {Trade} from '../pages/Trade';
 import {Portfolio} from '../pages/Portfolio';
 import colors from '../styles/colors';
 
+interface TabIconProps {
+  size: number;
+  color: string;
+  icon: SVGElement;
+}
+
+const HomeIcon = require('../assets/svg/home-icon.svg');
+const TradeIcon = require('../assets/svg/trade-icon.svg');
+const PortfolioIcon = require('../assets/svg/portfolio-icon.svg');
+
 const AppTab = createBottomTabNavigator();
+
+function TabIcon({size, color, icon}: TabIconProps) {
+  return (
+    <View>
+      <SvgUri width={size} height={size} fill={color} source={icon} />
+    </View>
+  );
+}
 
 export function AppRoutes() {
   return (
     <AppTab.Navigator
       screenOptions={{
         tabBarActiveTintColor: colors.purple,
+        tabBarInactiveTintColor: colors.text_dark,
       }}>
       <AppTab.Screen
         name="Home"
         component={Home}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({size, color}) => (
+            <TabIcon size={size} color={color} icon={HomeIcon} />
+          ),
+        }}
       />
 
       <AppTab.Screen
         name="Trade"
         component={Trade}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({size, color}) => (
+            <TabIcon size={size} color={color} icon={TradeIcon} />
+          ),
+        }}
       />
 
       <AppTab.Screen
         name="Portfolio"
         component={Portfolio}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({size, color}) => (
+            <TabIcon size={size} color={color} icon={PortfolioIcon} />
+          ),
+        }}
       />
     </AppTab.Navigator>
   );
