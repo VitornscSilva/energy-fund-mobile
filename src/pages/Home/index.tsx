bd90de5c57fb97a4c0ca738d2609874f238316d4
VitornscSilva <vitornscsilva.dev@gmail.com>
2023-05-30 16:31:53 -0300

    :sparkles: mocked Home page :sparkles:

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
+import {Card} from '../../components/Card';
+import {Divider} from '../../components/Divider';
+import {Funds} from './components/Funds';
+import {Header} from './components/Header';
+import {Container, BodyContainer} from './styles';
+
+const BusinessStatitics = require('../../assets/business-statistics.png');
 
 export function Home(): JSX.Element {
   return (
-    <View style={{alignSelf: 'center'}}>
-      <Text>Home</Text>
-    </View>
+    <ScrollView
+      keyboardShouldPersistTaps="handled"
+      contentContainerStyle={{flex: 1}}>
+      <Container>
+        <Header />
+        <Divider />
+        <BodyContainer>
+          <Funds />
+          <Card
+            title="Learn more about carbon credits"
+            description="Check out our top tips!"
+            image={BusinessStatitics}
+          />
+        </BodyContainer>
+      </Container>
+    </ScrollView>
   );
 }
